724201b08e5c4fa07202db24932817c491b68cfe
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-04-15 15:01:43 -0700

    develop: Oauth with github+google wth Authuser

--- server/auth.ts
+++ server/auth.ts
@@ -1,9 +1,21 @@
 import NextAuth from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/server";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   session: { strategy: "jwt" },
-  adapter: DrizzleAdapter(db),
-  providers: [],
+  adapter: DrizzleAdapter(db) as any, // any is a hack to get the types to work
+  providers: [
+    // can test with http://localhost:PORT/api/auth/signin
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
 });
