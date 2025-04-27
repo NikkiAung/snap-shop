c1cd69cad64dfd5d346433285c4854b98dc80106
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-04-26 22:42:14 -0700

    Developed : avator pfp

--- server/auth.ts
+++ server/auth.ts
@@ -17,10 +17,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID!,
       clientSecret: process.env.AUTH_GITHUB_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
     Credentials({
       authorize: async (credentials) => {
