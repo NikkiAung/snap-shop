7669ab14e31678efbdef7160bcf1213d8e19890a
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-04-28 21:39:58 -0700

    develop::frontend: pop up form mobile+web responsive

---
+++ components/hooks/useMediaQuery.ts
@@ -0,0 +1,14 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export function useMediaQuery(query: string) {
+  const [matches, setMatches] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(query);
+    const windowChangerHandler = () => setMatches(mediaQuery.matches);
+    setMatches(mediaQuery.matches);
+    mediaQuery.addEventListener("change", windowChangerHandler);
+    return () => mediaQuery.removeEventListener("change", windowChangerHandler);
+  }, [query]);
+  return matches;
+}
