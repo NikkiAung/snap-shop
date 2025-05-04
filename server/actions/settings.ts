1842681fff32f8a50db08113694879c7a49156e1
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-05-04 16:29:13 -0700

    fixBug: added revalidatePath to refresh the page for new display name

--- server/actions/settings.ts
+++ server/actions/settings.ts
@@ -5,6 +5,7 @@ import { db } from "..";
 import { eq } from "drizzle-orm";
 import { users } from "../schema";
 import { settingSchema } from "@/types/settings-schema";
+import { revalidatePath } from "next/cache";
 
 export const updateDisplayName = actionClient
   .schema(settingSchema)
@@ -17,5 +18,6 @@ export const updateDisplayName = actionClient
       .update(users)
       .set({ name: username })
       .where(eq(users.email, email));
+    revalidatePath("/dashboard/settings");
     return { success: "Display name updated!" };
   });
