d979e6de41e8df58e370e46364a0aa584dc1587b
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-05-04 16:26:18 -0700

    develop: extension OOP for session overriden

--- server/auth.ts
+++ server/auth.ts
@@ -11,6 +11,42 @@ import bcrypt from "bcrypt";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   session: { strategy: "jwt" },
+  callbacks: {
+    async session({ session, token }) {
+      if (session && token.sub) {
+        session.user.id = token.sub;
+      }
+      if (session.user && token.role) {
+        session.user.role = token.role as string;
+      }
+      if (session) {
+        session.user.isTwofactorEnabled = token.isTwofactorEnabled as boolean;
+        session.user.name = token.name as string;
+        session.user.email = token.email as string;
+        session.user.image = token.image as string;
+        session.user.isOauth = token.isOauth as boolean;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await db.query.users.findFirst({
+        where: eq(users.id, token.sub),
+      });
+      if (!existingUser) return token;
+      const existingAccount = await db.query.accounts.findFirst({
+        where: eq(accounts.userId, existingUser.id),
+      });
+
+      token.isOauth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
+      token.image = existingUser.image;
+      token.role = existingUser.role;
+      token.isTwofactorEnabled = existingUser.isTwoFactorEnabled;
+      return token;
+    },
+  },
   adapter: DrizzleAdapter(db) as any, // any is a hack to get the types to work
   providers: [
     // can test with http://localhost:PORT/api/auth/signin
