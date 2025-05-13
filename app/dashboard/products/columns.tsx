d7cf68ea46c3f6a75f45795cfc9f9eabb7782177
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-05-12 23:55:37 -0700

    Developed : row+col table for product display

---
+++ app/dashboard/products/columns.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontalIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+  id: number;
+  price: number;
+  title: string;
+  description: string;
+  image: string;
+  variants: any;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "image",
+    header: "Image",
+    cell: ({ row }) => {
+      const image = row.getValue("image") as string;
+      const title = row.getValue("title") as string;
+      return <Image src={image} alt={title} width={50} height={50} />;
+    },
+  },
+  {
+    accessorKey: "variants",
+    header: "Variants",
+  },
+  {
+    accessorKey: "title",
+    header: "Title",
+    cell: ({ row }) => {
+      const title = row.getValue("title") as string;
+      return <span className="text-sm font-medium">{title}</span>;
+    },
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("price"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+
+      return <span className="text-sm font-medium">{formatted}</span>;
+    },
+  },
+  {
+    accessorKey: "actions",
+    header: "Actions",
+    cell: () => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem className="cursor-pointer text-primary focus:bg-primary/20 focus:text-primary font-medium duration-300">
+              Edit Product
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer text-red-600 focus:bg-red-200 focus:text-red-600 font-medium duration-300">
+              Delete Product
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,7 +1,28 @@
+import { db } from "@/server";
 import React from "react";
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
+import placeHolderImage from "@/public/placeholder.jpg";
 
-const Products = () => {
-  return <div>Products</div>;
+const Products = async () => {
+  const products = await db.query.products.findMany({
+    orderBy: (products, { desc }) => [desc(products.id)],
+  });
+  console.log("products", products);
+  const productData = products.map((product) => ({
+    id: product.id,
+    price: product.price,
+    title: product.title,
+    description: product.description,
+    variants: [],
+    image: placeHolderImage.src,
+  }));
+  console.log(productData);
+  return (
+    <main>
+      <DataTable columns={columns} data={productData} />
+    </main>
+  );
 };
 
 export default Products;
