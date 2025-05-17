75325b4a7e00977068703f9eee8286b15acb2ef8
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-05-17 14:34:24 -0700

    Developed : variants display

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -6,17 +6,33 @@ import placeHolderImage from "@/public/placeholder.jpg";
 
 const Products = async () => {
   const products = await db.query.products.findMany({
+    with: {
+      productVariants: { with: { variantImages: true, variantTags: true } },
+    },
     orderBy: (products, { desc }) => [desc(products.id)],
   });
   console.log("products", products);
-  const productData = products.map((product) => ({
-    id: product.id,
-    price: product.price,
-    title: product.title,
-    description: product.description,
-    variants: [],
-    image: placeHolderImage.src,
-  }));
+  const productData = products.map((product) => {
+    if (product.productVariants.length === 0) {
+      return {
+        id: product.id,
+        price: product.price,
+        title: product.title,
+        description: product.description,
+        variants: [],
+        image: placeHolderImage.src,
+      };
+    }
+    // Need to handle the case when productVariants.length > 0
+    return {
+      id: product.id,
+      price: product.price,
+      title: product.title,
+      description: product.description,
+      variants: product.productVariants,
+      image: product.productVariants[0]?.variantImages[0]?.image_url,
+    };
+  });
   console.log(productData);
   return (
     <main>
